6edd8468e1f869dba08880d1d7ef6909ed3dbec3
Sohil Khan <sohilkhanchp@gmail.com>
2024-04-13 15:37:06 +0530

    #update

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import img3 from "../Images/Busness_Card.png"
-import img4 from "../Images/Busness_Card1.png"
+import img2 from "../Images/Busness_Card.png"
+import img3 from "../Images/Rat in a maze.png"
+import img4 from "../Images/Snake Game.png"
+import img5 from "../Images/Dad joke Generater.png"
+import img6 from "../Images/Tinder Clone.png"
+import img7 from "../Images/NGO.png"
 import PortfolioItem from './PortfolioItem';
 
 const Portfolio = () => {
@@ -9,37 +13,37 @@ const Portfolio = () => {
       <h2 className="heading">My <span>Portfolio</span></h2>
       <div className="portfolio-container">
         <PortfolioItem
-          image={img3}
+          image={img2}
           title="Project 1"
           description="Myntra App Clone."
           id="https://splendid-centaur-e9c540.netlify.app/"
         />
         <PortfolioItem
-          image={img4}
+          image={img3}
           title="Project 2"
           description="Rat In a Maze."
           id="https://rat-in-a-maze2.netlify.app"
         />
         <PortfolioItem
-          image={img3}
+          image={img4}
           title="Project 3"
           description="Snake Game."
           id="https://sanpkakhel.netlify.app"
         />
         <PortfolioItem
-          image={img4}
+          image={img5}
           title="Project 4"
           description="Dad Joke Generater."
           id="https://dad-joke-genrator.netlify.app"
         />
         <PortfolioItem
-          image={img3}
+          image={img6}
           title="Project 5"
           description="Tinder-Clone."
           id="https://tinderclone-html.netlify.app"
         />
         <PortfolioItem
-          image={img4}
+          image={img7}
           title="Project 6"
           description="NGO Website."
           id="https://tangerine-marzipan-7b822b.netlify.app"
