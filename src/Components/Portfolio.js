2b4e1c1a95d1e942501f1195525bbf7cce028166
Sohil Khan <sohilkhanchp@gmail.com>
2024-04-13 14:07:10 +0530

    #update

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -11,38 +11,38 @@ const Portfolio = () => {
         <PortfolioItem
           image={img3}
           title="Project 1"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, molestias."
-          id="https://dad-joke-genrator.netlify.app"
+          description="Myntra App Clone."
+          id="https://splendid-centaur-e9c540.netlify.app/"
         />
         <PortfolioItem
           image={img4}
           title="Project 2"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, molestias."
-          id="#ViewProject"
+          description="Rat In a Maze."
+          id="https://rat-in-a-maze2.netlify.app"
         />
         <PortfolioItem
           image={img3}
           title="Project 3"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, molestias."
-          id="#ViewProject"
+          description="Snake Game."
+          id="https://sanpkakhel.netlify.app"
         />
         <PortfolioItem
           image={img4}
           title="Project 4"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, molestias."
-          id="#ViewProject"
+          description="Dad Joke Generater."
+          id="https://dad-joke-genrator.netlify.app"
         />
         <PortfolioItem
           image={img3}
           title="Project 5"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, molestias."
-          id="#ViewProject"
+          description="Tinder-Clone."
+          id="https://tinderclone-html.netlify.app"
         />
         <PortfolioItem
           image={img4}
           title="Project 6"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, molestias."
-          id="#ViewProject"
+          description="NGO Website."
+          id="https://tangerine-marzipan-7b822b.netlify.app"
         />
       </div>
     </section>
